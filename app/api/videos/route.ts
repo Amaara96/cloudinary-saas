8d92ad39b586bbabc8fdd7f3bc9e5821973bd877
Amaara96 <amara.alex96@gmail.com>
2026-06-08 05:55:44 +0100

    adapter

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -1,17 +1,30 @@
-import { NextRequest, NextResponse } from "next/server"
-import { PrismaClient } from "@prisma/client/extension"
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prisma = new PrismaClient() 
 
-export async function GET(request: NextRequest){
-    try {
-        const videos = await prisma.video.findMany({
-            orderBy: {createdAt: "desc"}
-        })
-        return NextResponse.json(videos)
-    } catch (error) {
-        return  NextResponse.json({error: "Error fectching videos"}, {status: 500})
-    } finally{
-        await prisma.$disconnect()
-    }
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+
+
+export async function GET() {
+  try {
+    const videos = await prisma.video.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return NextResponse.json(videos);
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { error: "Error fetching videos" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
 }

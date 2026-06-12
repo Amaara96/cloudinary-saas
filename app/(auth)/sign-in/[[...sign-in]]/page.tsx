202457ab3ea5c1c43a6b3c67840a436a7b96799f
Amaara96 <amara.alex96@gmail.com>
2026-06-12 14:52:01 +0100

    black

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,4 +2,4 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function SignInPage() {
   return <SignIn />
-}
+}                                                                                                                                                                                                                                                                                                                                                                                                              
